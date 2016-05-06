2c2329a23ecd92fe8165fd3d393d04d84904cc9a
kai peng <695326874@qq.com>
2016-05-06 15:21:20 +0800

    Initial commit

---
+++ app/src/main/java/com/parker/droidanti/MainActivity.java
@@ -0,0 +1,13 @@
+package com.parker.droidanti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
