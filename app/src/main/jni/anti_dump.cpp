4a70a98e2f2d5700bcb31c995c82aa742f4b7eb7
kai peng <695326874@qq.com>
2016-05-06 15:31:21 +0800

    add anti_dump module

---
+++ app/src/main/jni/anti_dump.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pengk on 2016/5/6.
+//
+
+#include "anti_dump.h"
---
+++ app/src/main/jni/anti_dump.h
@@ -0,0 +1,8 @@
+//
+// Created by pengk on 2016/5/6.
+//
+
+#ifndef DROIDANTI_ANTI_DUMP_H
+#define DROIDANTI_ANTI_DUMP_H
+
+#endif //DROIDANTI_ANTI_DUMP_H
