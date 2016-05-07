85cd53ff41b851c43b950a65d610d371ecd56eaf
parkerpeng <2324503060@qq.com>
2016-05-07 12:09:17 +0800

    add anti_debug module

---
+++ app/src/main/jni/anti_debug.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2016/5/7 0007.
+//
+
+#include "anti_debug.h"
---
+++ app/src/main/jni/anti_debug.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2016/5/7 0007.
+//
+
+#ifndef DROIDANTI_ANTI_DEBUG_H
+#define DROIDANTI_ANTI_DEBUG_H
+
+#endif //DROIDANTI_ANTI_DEBUG_H
